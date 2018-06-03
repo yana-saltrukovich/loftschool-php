8fa0df4b39bcc542f42fcbe057a4779ecbde19db
Yana <saltrukovich-yana@yandex.ru>
2018-06-03 14:25:29 +0500

    fix: edit to psr2

--- Untitled-1.php
+++ Untitled-1.php
@@ -1,7 +1,11 @@
 <?php
 const TEST_CONSTANTA = 'asd';
-$user_name = "Igor";
-function showSomething(){
-     RETURN 'SOMETHING';
-    };
-if (1==1) echo 'hi';
+$userName = "Igor";
+function showSomething()
+{
+    return 'SOMETHING';
+}
+
+if (1 == 1) {
+    echo 'hi';
+}
